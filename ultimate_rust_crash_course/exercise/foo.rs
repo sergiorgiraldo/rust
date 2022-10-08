d69de92b84b81ad24f14ad151755edc82ff981d4
Sergio Giraldo <sergio@giraldo.com.br>
2022-10-08 13:05:33 +0200

    ownership:: by sergio giraldo @ 20221008T1305CEST, gpg signed

---
+++ ultimate_rust_crash_course/exercise/foo.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let mut s1 = String::from("abc");
+    println!("{}", s1);
+    d(&mut s1);
+    println!("{}", s1);
+
+    fn d(s: &mut String) {
+        s.insert_str(0, "Hi, ");
+    }
+
+}
