a414b869f4f14e765c5d87d8a1bdb3c37c89b352
Sergio Giraldo <sergio@giraldo.com.br>
2022-10-23 17:38:10 +0200

    problem 5:: by sergio giraldo @ 20221023T1738CEST, gpg signed

--- euler/src/main.rs
+++ euler/src/main.rs
@@ -1,8 +1,20 @@
+use std::time::SystemTime;
+
 mod euler;
+
+fn timeit<F: Fn() -> T, T>(what: &str, f: F) -> T {
+    let start = SystemTime::now();
+    let result = f();
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).unwrap();
+    println!("\t{} took {} msecs", what, duration.as_millis());
+    result
+  }
 fn main() {
     println!("Euler problems");
-    euler::p001::do_it();
-    euler::p002::do_it();
-    euler::p003::do_it();
-    euler::p004::do_it();
+    timeit("p001", || euler::p001::do_it());
+    timeit("p002", || euler::p002::do_it());
+    timeit("p003", || euler::p003::do_it());
+    timeit("p004", || euler::p004::do_it());
+    timeit("p005", || euler::p005::do_it());
 }
