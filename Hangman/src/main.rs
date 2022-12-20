c55cee89fcfdc4e0c64a78ea0ee7a98e8a6fb4f8
Sergio Giraldo <sergio@giraldo.com.br>
2022-12-20 11:34:26 +0100

    cosmetics to hangman:: by sergio giraldo @ 20221220T1134CET, gpg signed

--- Hangman/src/main.rs
+++ Hangman/src/main.rs
@@ -1,23 +1,26 @@
-extern crate rand;
 extern crate ansi_term;
+extern crate rand;
 
 use std::process::Command;
 
-use std::io;
-use std::io::BufReader;
-use std::io::BufRead;
-use std::fs::File;
+use ansi_term::Colour::Green;
+use ansi_term::Colour::Red;
+use ansi_term::Colour::Yellow;
 use rand::prelude::*;
 use rand::seq::sample_iter;
-use ansi_term::Colour::Red;
-use ansi_term::Colour::Green;
-use ansi_term::Colour::Yellow;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
 
 struct GameData {
-    secret_line         : String,
-    discovered_letters  : String,
-    lives               : i32,
-    status              : String
+    secret_line: String,
+    discovered_letters: String,
+    guessed_letters: String,
+    missed_letters: String,
+    history_letters: String,
+    lives: i32,
+    status: String,
 }
 
 enum UserInputStatus {
@@ -26,158 +29,171 @@ enum UserInputStatus {
     LetterMissed,
 }
 
-fn main()
-{
+fn main() {
     let random_line = get_random_line().expect("Failed to read input data!");
 
-    let mut gd : GameData = GameData {
-                        secret_line        : random_line,
-                        discovered_letters : String::new(),
-                        lives              : 5,
-                        status             : String::new()
-                        };
+    let mut gd: GameData = GameData {
+        secret_line: random_line,
+        discovered_letters: String::new(),
+        guessed_letters: String::new(),
+        missed_letters: String::new(),
+        history_letters: String::new(),
+        lives: 5,
+        status: String::new(),
+    };
 
     let mut secret_line_masked = format_masked_string(&gd.secret_line, &gd.discovered_letters);
 
-    loop
-    {
+    loop {
         update_screen(&gd, &secret_line_masked);
 
         println!("Type your guess:");
         let user_guess = read_guess();
 
-        if validate_user_guess(user_guess)
-        {
+        if validate_user_guess(user_guess) {
             let guess_lower = user_guess.unwrap().to_lowercase().next().unwrap();
 
-            match check_user_guess(&gd, guess_lower)
-            {
-                UserInputStatus::LetterGuessed =>
-                {
+            match check_user_guess(&gd, guess_lower) {
+                UserInputStatus::LetterGuessed => {
                     gd.discovered_letters.push(guess_lower);
+                    gd.guessed_letters.push(guess_lower);
                     let status = format!("You discovered {}", guess_lower);
                     gd.status = Green.paint(status).to_string();
-                    secret_line_masked = format_masked_string(&gd.secret_line, &gd.discovered_letters);
+                    gd.history_letters = format!(
+                        "All letters: {}\nGuessed letters: {}\nMissed letters: {}",
+                        gd.discovered_letters,
+                        Green.paint(&gd.guessed_letters),
+                        Red.paint(&gd.missed_letters)
+                    );
+                    secret_line_masked =
+                        format_masked_string(&gd.secret_line, &gd.discovered_letters);
 
-                    if !secret_line_masked.contains('_')
-                    {
+                    if !secret_line_masked.contains('_') {
                         gd.status = Green.bold().paint("You won!").to_string();
                         update_screen(&gd, &secret_line_masked);
                         break;
                     }
                 }
 
-                UserInputStatus::LetterMissed =>
-                {
+                UserInputStatus::LetterMissed => {
                     gd.discovered_letters.push(guess_lower);
                     gd.lives = gd.lives - 1;
-
-                    if gd.lives == 0
-                    {
+                    gd.missed_letters.push(guess_lower);
+                    if gd.lives == 0 {
                         gd.status = Red.bold().paint("You lost!").to_string();
+                        gd.history_letters = format!(
+                            "All letters: {}\nGuessed letters: {}\nMissed letters: {}",
+                            gd.discovered_letters,
+                            Green.paint(&gd.guessed_letters),
+                            Red.paint(&gd.missed_letters)
+                        );
                         secret_line_masked = format_masked_string(&gd.secret_line, &gd.secret_line);
                         update_screen(&gd, &secret_line_masked);
                         break;
-                    }
-                    else
-                    {
+                    } else {
                         let status = format!("Unfortunately, no {}", guess_lower);
+                        gd.history_letters = format!(
+                            "All letters: {}\nGuessed letters: {}\nMissed letters: {}",
+                            gd.discovered_letters,
+                            Green.paint(&gd.guessed_letters),
+                            Red.paint(&gd.missed_letters)
+                        );
                         gd.status = Red.paint(status).to_string();
+                        update_screen(&gd, &secret_line_masked);
                     }
                 }
 
-                UserInputStatus::AlreadyDiscovered =>
-                {
+                UserInputStatus::AlreadyDiscovered => {
                     let status = format!("{} is already discovered!", guess_lower);
                     gd.status = Yellow.paint(status).to_string();
                 }
             }
-        }
-        else
-        {
+        } else {
             let status = format!("It is not a letter!");
             gd.status = Yellow.paint(status).to_string();
         }
     }
 }
 
-fn read_guess() -> Option<char>
-{
+fn read_guess() -> Option<char> {
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
     guess.trim().chars().nth(0)
 }
 
-fn get_random_line() -> Result<String, io::Error>
-{
+fn get_random_line() -> Result<String, io::Error> {
     let f = File::open("input.txt")?;
     let file = BufReader::new(&f);
     let mut rng = thread_rng();
-    let sample = sample_iter(&mut rng, file.lines(), 1).unwrap().pop().unwrap().unwrap();
+    let sample = sample_iter(&mut rng, file.lines(), 1)
+        .unwrap()
+        .pop()
+        .unwrap()
+        .unwrap();
     let secret_line = sample.to_lowercase();
     Ok(secret_line)
 }
 
-fn format_masked_string(input: &String, mask: &String) -> String
-{
-    let mut result : String = String::new();
+fn format_masked_string(input: &String, mask: &String) -> String {
+    let mut result: String = String::new();
 
-    for (_u, c) in input.chars().enumerate()
-    {
-        result.push(if c == ' ' {c}
-            else if mask.contains(c) {c}
-            else {'_'});
+    for (_u, c) in input.chars().enumerate() {
+        result.push(if c == ' ' {
+            c
+        } else if mask.contains(c) {
+            c
+        } else {
+            '_'
+        });
         result.push(' ');
     }
 
     result
 }
 
-fn validate_user_guess(user_guess: Option<char>) -> bool
-{
-    match user_guess
-    {
-        Some(guess) =>
-        {
-            if !guess.is_alphabetic() { false }
-            else { true }
+fn validate_user_guess(user_guess: Option<char>) -> bool {
+    match user_guess {
+        Some(guess) => {
+            if !guess.is_alphabetic() {
+                false
+            } else {
+                true
+            }
         }
 
-        None => { return false; }
+        None => {
+            return false;
+        }
     }
 }
 
-fn check_user_guess(gd: &GameData, user_guess: char) -> UserInputStatus
-{
-    if gd.discovered_letters.contains(user_guess)
-    {
+fn check_user_guess(gd: &GameData, user_guess: char) -> UserInputStatus {
+    if gd.discovered_letters.contains(user_guess) {
         return UserInputStatus::AlreadyDiscovered;
     }
 
-    if !gd.secret_line.contains(user_guess)
-    {
+    if !gd.secret_line.contains(user_guess) {
         return UserInputStatus::LetterMissed;
     }
 
     UserInputStatus::LetterGuessed
 }
 
-fn update_screen(gd: &GameData, secret_line: &String)
-{
+fn update_screen(gd: &GameData, secret_line: &String) {
     clear();
     println!("HANGMAN: CAN YOU GUESS THE SENTENCE?");
-    println!("Lives: {}. Discovered letters: {}", gd.lives, gd.discovered_letters);
+    println!("Lives: {}", gd.lives);
+    println!("{}", gd.history_letters);
     print_hangman(&gd);
     println!("{}", secret_line);
     println!("{}", gd.status);
 }
 
-fn print_hangman(gd: &GameData)
-{
-    match gd.lives
-    {
-        0 =>
-        {
+fn print_hangman(gd: &GameData) {
+    match gd.lives {
+        0 => {
             println!(" _________   ");
             println!("|         |  ");
             println!("|         XO ");
@@ -187,8 +203,7 @@ fn print_hangman(gd: &GameData)
             println!("|            ");
         }
 
-        1 =>
-        {
+        1 => {
             println!(" _________   ");
             println!("|         |  ");
             println!("|         O  ");
@@ -198,8 +213,7 @@ fn print_hangman(gd: &GameData)
             println!("|        ||| ");
         }
 
-        2 =>
-        {
+        2 => {
             println!(" _________   ");
             println!("|            ");
             println!("|         O  ");
@@ -209,8 +223,7 @@ fn print_hangman(gd: &GameData)
             println!("|        ||| ");
         }
 
-        3 =>
-        {
+        3 => {
             println!(" _________   ");
             println!("|            ");
             println!("|            ");
@@ -218,11 +231,9 @@ fn print_hangman(gd: &GameData)
             println!("|        /|\\ ");
             println!("|        / \\ ");
             println!("|        ||| ");
-
         }
 
-        4 =>
-        {
+        4 => {
             println!(" _________   ");
             println!("|            ");
             println!("|            ");
@@ -232,8 +243,7 @@ fn print_hangman(gd: &GameData)
             println!("|        / \\ ");
         }
 
-        _ =>
-        {
+        _ => {
             println!("             ");
             println!("             ");
             println!("             ");
@@ -245,11 +255,9 @@ fn print_hangman(gd: &GameData)
     }
 }
 
-fn clear()
-{
-  let output = Command::new("clear").output().unwrap_or_else(|e|{
-    panic!("failed to execute process: {}", e)
-  });
-  println!("{}", String::from_utf8_lossy(&output.stdout));
+fn clear() {
+    let output = Command::new("clear")
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+    println!("{}", String::from_utf8_lossy(&output.stdout));
 }
-
