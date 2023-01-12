4a6a362b73b6045d29d696828e5f14a7a60d1649
Sergio Giraldo <sergio@giraldo.com.br>
2023-01-12 14:55:23 +0100

    data structures: functions:: by sergio giraldo @ 20230112T1455CET, gpg signed

---
+++ data-structures/functions/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    println!("######## Fibonacci");
+    println!("fib(1) => {}", fibonacci(1));
+    println!("fib(2) => {}", fibonacci(2));
+    println!("fib(3) => {}", fibonacci(3));
+    println!("fib(4) => {}", fibonacci(4));
+    println!("fib(5) => {}", fibonacci(5));
+
+    println!("nCr(4,2) => {}", n_cr(4, 2));
+}
+
+fn fibonacci(n: i32) -> i32 {
+    if n < 0 {
+        panic!("{} is negative!", n);
+    }
+    match n {
+        0 => panic!("zero is not a right argument to fibonacci()!"),
+        1 | 2 => 1,
+        3 => 2,
+        /*
+        50    => 12586269025,
+        */
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+fn factorial(num: i32) -> i32 {
+    match num {
+        0 | 1 => 1,
+        _ => factorial(num - 1) * num,
+    }
+}
+
+fn n_cr(n: i32, r: i32) -> i32 {
+    println!("######## find the number of possible combinations that can be obtained by taking a sample of items from a larger set");
+    let ans = factorial(n) / (factorial(r) * factorial(n - r));
+    ans
+}
